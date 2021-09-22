9a0dc60a3c71c0fe47fecc4827f1f1b5ed43b5b9
blake-hartung <blakehartung23@gmail.com>
2021-09-22 11:11:14 -0400

    problem 2 mostly complete

--- analysis.R
+++ analysis.R
@@ -6,8 +6,10 @@ library(openintro)
 data(fastfood)
 
 # Horizontal boxplots categorized by restaurant
-boxplot_by_rest <- ggplot(fastfood, aes(x=calories, y=restaurant)) +
-  geom_boxplot()
+boxplot_by_rest <- ggplot(fastfood, aes(x=calories,
+                                        y=reorder(restaurant, calories, median))) +
+  geom_boxplot() +
+  ylabb("restaurant")
 
 # create hist
 histograms <- ggplot(fastfood, aes(x=calories)) +
@@ -20,6 +22,7 @@ histograms <- ggplot(fastfood, aes(x=calories)) +
 crisp <- fastfood %>%
   filter((str_detect(item, "Crispy")) == T) %>%
   mutate(cooktype = "Crispy")
+
 grilled <- fastfood %>%
   filter((str_detect(item, "Grilled")) == T) %>%
   mutate(cooktype = "Grilled")
@@ -28,32 +31,66 @@ grilled <- fastfood %>%
 crisp_and_grilled <- rbind(crisp, grilled)
 
 # create density plot
-dens_comparison <- ggplot(crisp_and_grilled, aes(x=calories, color=cooktype)) +
-  geom_density()
+dens_comparison <- ggplot(crisp_and_grilled, aes(x=calories,
+                                                 color=cooktype,
+                                                 fill=cooktype)) +
+  geom_density(alpha=0.2)
+
+### problem 2
+data(mtl)
+
+gg_hist <- ggplot(mtl) +
+  geom_histogram(aes(x=asubic),
+                 fill="gray",
+                 color="black")
+
+default_hist <- hist(mtl$asubic)
+
+gg_left <- ggplot(mtl) +
+  geom_histogram(aes(x = age),
+                 fill = "gray",
+                 color = "black",
+                 binwidth = 5,
+                 boundary = 40,
+                 closed = "left") +
+  scale_x_continuous(breaks = seq(40, 80, by = 5))
+
+gg_right <- ggplot(mtl) +
+  geom_histogram(aes(x = age),
+                 fill = "gray",
+                 color = "black",
+                 binwidth = 5,
+                 boundary = 40,
+                 closed = "right") +
+  scale_x_continuous(breaks = seq(40, 80, by = 5))
+
+# change boundary to just above the discrete value so now boundary value is included
+gg_left_changed <- ggplot(mtl) +
+  geom_histogram(aes(x = age),
+                 fill = "gray",
+                 color = "black",
+                 binwidth = 5,
+                 boundary = 40.01,
+                 closed = "left") +
+  scale_x_continuous(breaks = seq(40, 80, by = 5))
+
 
 ### problem 4
 # import breslow from boot
 library(boot)
 data(breslow)
 
-# dataframe filtered for non smokers and plotted
-non_smokers <- ggplot(breslow %>% filter(smoke == 0), aes(x=age, y=y)) +
-  geom_histogram(stat='identity') +
-  ylim(0,225) +
-  xlab("Age (midpoint of 10 yr bins)") +
-  ylab("") +
-  ggtitle("Deaths due to coronary artery diease in non-smokers")
+# Mutate dataset for labelling purposes
+breslow_mutate <- breslow %>%
+  mutate(labeled = ifelse(smoke == 1, "Smoker", "Non-Smoker"))
 
-# and filtered for smokers
-smokers <- ggplot(breslow %>% filter(smoke == 1), aes(x=age, y=y)) +
+# plot the comparison
+death_comparison <- ggplot(breslow_mutate, aes(x=age, y=y)) +
   geom_col() +
-  ylim(0, 225) +
-  xlab("Age (midpoint of 10 yr bins") +
-  ylab("") +
-  ggtitle("Deaths due to coronary artery diease in smokers")
-
-# arrange the histograms onto the same page
-arranged_plots <- grid.arrange(non_smokers, smokers, nrow=1)
+  ylim(0, 215) +
+  xlab("Age (midpoint of 10 yr bins)") +
+  ylab("Deaths due to Coronary Artery Disease") +
+  facet_wrap(labeled ~ .)
 
 ### problem 5
 library(ggridges)
@@ -63,11 +100,18 @@ quick_histogram <- ggplot(loans_full_schema, aes(x=loan_amount)) +
   geom_histogram(binwidth=2500, fill='gray', color='black') +
   geom_density(aes(y=2500 * ..count..), color='red')
 
-boxplot_data <- ggplot(loans_full_schema, aes(x=loan_amount, y=loan_purpose)) +
-  geom_boxplot()
+boxplot_data <- ggplot(loans_full_schema,
+                       aes(x=loan_amount,
+                           y=reorder(loan_purpose, loan_amount, median))) +
+  geom_boxplot() +
+  ylab("Loan Purpose") +
+  xlab("Loan Amount")
 
-ridge_data <- ggplot(loans_full_schema, aes(x=loan_amount, y=loan_purpose)) +
-  geom_density_ridges(bandwidth=2500)
-
+ridge_data <- ggplot(loans_full_schema,
+                     aes(x=loan_amount,
+                         y=reorder(loan_purpose, loan_amount, median))) +
+  geom_density_ridges(bandwidth=2500) +
+  ylab("Loan Purpose") +
+  xlab("Loan Amount")
 
 
